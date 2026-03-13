36ecacf117ce42c5b8d6404785daa5c0f07adb33
Alejandro <ale.torrubiano@gmail.com>
2026-03-13 21:16:19 +0100

    chore: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,34 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error',
+}
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    settings: {
+      react: { version: '19' },
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    rules: {
+      'no-unused-vars': [
+        RULES.ERROR,
+        { args: 'all', argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' },
+      ],
+      'object-curly-spacing': [RULES.ERROR, 'always'],
+      'array-callback-return': [RULES.OFF, { checkForEach: true }],
+      'no-return-assign': RULES.OFF,
+      'no-undef': RULES.OFF,
+      '@typescript-eslint/no-unused-vars': RULES.OFF,
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
